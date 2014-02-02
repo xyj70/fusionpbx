a93bf1b034a8393036cea91624038cd9a01d410b
r.neese@gmail.com <r.neese@gmail.com@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2014-02-02 23:02:12 +0000

    updating core build script

--- branches/dev/Debian-pkg-scripts/fusionpbx-core/build-fusionpbx-core.sh
+++ branches/dev/Debian-pkg-scripts/fusionpbx-core/build-fusionpbx-core.sh
@@ -6,15 +6,7 @@ svn export http://fusionpbx.googlecode.com/svn/branches/dev/fusionpbx /usr/src/f
 
 cd /usr/src/fusionpbx-core
 
-for i in adminer call_clock call_broadcast call_center call_center_active call_flows calls_active click_to_call \
-         confrenece_center conferences conferences_active contacts content devices dialplanfratures edit exec fax \
-         fifo fifo_list follow_me hot_desking ivr_menu meeting park provisioning recordings ring_groups schemas services \
-         sql_query time_conditions traffic_graph xmpp
-do rm -rf app/"${i}"
-done        
-  
-mkdir /usr/src/fusionpbx-themes
-for i in accessible clasic default enhanced nature ;do rm-rf app/"${i}" ;done        
+rm -rf app/* themes/*       
 
 dpkg-buildpackage -rfakeroot -i
 
