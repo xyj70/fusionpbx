a178f31bce752f52b54ae105c895eda11ba7aee5
r.neese@gmail.com <r.neese@gmail.com@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2014-02-10 21:09:41 +0000

    update to new build script. almost ready to roll and get new pkgs going

--- branches/dev/Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
+++ branches/dev/Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rm -rf /usr/src/fusionpbx-pkgs-build
+PKGVER=3.5-1
+TIME=$(date)
+REP0=stable # stable or devel
+WRKDIR=/usr/src/fusionpbx-pkgs-build
 
-wrk_dir=/usr/src/fusionpbx-pkgs-build
+rm -rf /usr/src/fusionpbx-pkgs-build
 
 build_stable_pkgs="n"
 
@@ -12,14 +15,14 @@ svn_src=http://fusionpbx.googlecode.com/svn/branches/dev
 fi
 
 #get pkg system scripts
-svn export http://fusionpbx.googlecode.com/svn/branches/dev/Debian-pkg-scripts "$wrk_dir"
+svn export http://fusionpbx.googlecode.com/svn/branches/dev/Debian-pkg-scripts "$WRKDIR"
 
 #get src for core
-svn export --force "$svn_src"/fusionpbx "$wrk_dir"/fusionpbx-core
+svn export --force "$svn_src"/fusionpbx "$WRKDIR"/fusionpbx-core
 
 #get src for theme
 for i in accessible classic default enhanced nature
-do svn export "$svn_src"/fusionpbx/themes/"${i}" "$wrk_dir"/fusionpbx-themes/fusionpbx-theme-"${i}"/"${i}"
+do svn export "$svn_src"/fusionpbx/themes/"${i}" "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i}"/"${i}"
 done
 
 #get src for apps
@@ -30,21 +33,53 @@ dialplan_outbound edit exec extensions fax fifo fifo_list follow_me gateways hot
 ivr_menu login log_viewer meetings modules music_on_hold park provision recordings \
 registrations ring_groups schemas services settings sip_profiles sip_status sql_query \
 system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr xmpp
-do svn export "$svn_src"/fusionpbx/app/"${i}" "$wrk_dir"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
+do svn export "$svn_src"/fusionpbx/app/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
 done
 
 #Build pkgs
 #SET Version nmbr in debian/changelog
+cat > "$WRKDIR"/fusionpbx-core/debian/changelog <DELIM
+fusionpbx-core" ($PKGVER) stable; urgency=low
 
+  * new deb pkg for fusionpbx-core
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+
+for i in accessible classic default enhanced nature
+do cat > "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i//_/-}"/debian/changelog <DELIM
+fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
+
+  * new deb pkg for fusionpbx-theme-"${i//_/-}"
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+done
+
+for i in adminer call_block call_broadcast call_center call_center_active call_flows calls \
+calls_active click_to_call conference_centers conferences conferences_active \
+contacts content destinations devices dialplan dialplan_features dialplan_inbound \
+dialplan_outbound edit exec extensions fax fifo fifo_list follow_me gateways hot_desking \
+ivr_menu login log_viewer meetings modules music_on_hold park provision recordings \
+registrations ring_groups schemas services settings sip_profiles sip_status sql_query \
+system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr xmpp
+do cat > "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/debian/changelog <DELIM
+fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
+
+  * new deb pkg for fusionpbx-app-"${i//_/-}"
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+done
 
 #build core pkg
-cd "$wrk_dir"/fusionpbx-core
+cd "$WRKDIR"/fusionpbx-core
 rm -rf app/* themes/*
 dpkg-buildpackage -rfakeroot -i
 
 #build theme pkgs
 for i in accessible classic default enhanced nature
-do cd "$wrk_dir"/fusionpbx-themes/fusionpbx-theme-"${i}"
+do cd "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i}"
 dpkg-buildpackage -rfakeroot -i
 done
 
@@ -56,8 +91,19 @@ dialplan-outbound edit exec extensions fax fifo fifo-list follow-me gateways hot
 ivr-menu login log-viewer meetings modules music-on-hold park provision recordings \
 registrations ring-groups schemas services settings sip-profiles sip-status sql-query \
 system time-conditions traffic-graph vars voicemail-greetings voicemails xml-cdr xmpp
-do cd "$wrk_dir"/fusionpbx-apps/fusionpbx-app-"${i}" 
+do cd "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i}" 
 dpkg-buildpackage -rfakeroot -i
 done
 
+cd "$WRKDIR"
+mkdir -p "$WRKDIR"/debs-fusionpbx-$PKGVER-wheezy
 
+
+mv *.deb debs-fusionpbx-$PKGVER-wheezy
+mv *.changes debs-fusionpbx-$PKGVER-wheezy
+mv *.xz debs-fusionpbx-$PKGVER-wheezy
+mv *.dsc debs-fusionpbx-$PKGVER-wheezy
+
+cp -rp "$WRKDIR"/debs-fusionpbx-$PKGVER-wheezy/* "$REPO"/incoming
+
+cd "$REPO" && ./import-new-pkgs.sh
