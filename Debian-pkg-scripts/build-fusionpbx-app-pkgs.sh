435ecd60cb51f2d3ca690a616340112a46968c71
r.neese <r.neese@gmail.com>
2014-02-10 23:05:35 +0000

    update the build script

--- Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
+++ Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
@@ -17,6 +17,41 @@ fi
 #get pkg system scripts
 svn export http://fusionpbx.googlecode.com/svn/branches/dev/Debian-pkg-scripts "$WRKDIR"
 
+#SET Version nmbr in debian/changelog
+cat > "$WRKDIR"/fusionpbx-core/debian/changelog << DELIM
+fusionpbx-core" ($PKGVER) stable; urgency=low
+
+  * new deb pkg for fusionpbx-core
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+
+for i in accessible classic default enhanced nature
+do cat > "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i//_/-}"/debian/changelog << DELIM
+fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
+
+  * new deb pkg for fusionpbx-theme-"${i//_/-}"
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+done
+
+for i in adminer call_block call_broadcast call_center call_center_active call_flows calls \
+calls_active click_to_call conference_centers conferences conferences_active \
+contacts content destinations devices dialplan dialplan_inbound \
+dialplan_outbound edit exec extensions fax fifo fifo_list follow_me gateways hot_desking \
+ivr_menu login log_viewer meetings modules music_on_hold park provision recordings \
+registrations ring_groups schemas services settings sip_profiles sip_status sql_query \
+system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr xmpp
+do cat > "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/debian/changelog << DELIM
+fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
+
+  * new deb pkg for fusionpbx-app-"${i//_/-}"
+
+ -- Richard Neese <r.neese@gmail.com> $TIME -0500
+DELIM
+done
+
 #get src for core
 svn export --force "$svn_src"/fusionpbx "$WRKDIR"/fusionpbx-core
 
@@ -37,41 +72,6 @@ do svn export "$svn_src"/fusionpbx/app/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx
 done
 
 #Build pkgs
-#SET Version nmbr in debian/changelog
-cat > "$WRKDIR"/fusionpbx-core/debian/changelog <DELIM
-fusionpbx-core" ($PKGVER) stable; urgency=low
-
-  * new deb pkg for fusionpbx-core
-
- -- Richard Neese <r.neese@gmail.com> $TIME -0500
-DELIM
-
-for i in accessible classic default enhanced nature
-do cat > "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i//_/-}"/debian/changelog <DELIM
-fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
-
-  * new deb pkg for fusionpbx-theme-"${i//_/-}"
-
- -- Richard Neese <r.neese@gmail.com> $TIME -0500
-DELIM
-done
-
-for i in adminer call_block call_broadcast call_center call_center_active call_flows calls \
-calls_active click_to_call conference_centers conferences conferences_active \
-contacts content destinations devices dialplan dialplan_inbound \
-dialplan_outbound edit exec extensions fax fifo fifo_list follow_me gateways hot_desking \
-ivr_menu login log_viewer meetings modules music_on_hold park provision recordings \
-registrations ring_groups schemas services settings sip_profiles sip_status sql_query \
-system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr xmpp
-do cat > "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/debian/changelog <DELIM
-fusionpbx-app-"${i//_/-}" ($PKGVER) stable; urgency=low
-
-  * new deb pkg for fusionpbx-app-"${i//_/-}"
-
- -- Richard Neese <r.neese@gmail.com> $TIME -0500
-DELIM
-done
-
 #build core pkg
 cd "$WRKDIR"/fusionpbx-core
 rm -rf app/* themes/*
@@ -98,11 +98,13 @@ done
 cd "$WRKDIR"
 mkdir -p "$WRKDIR"/debs-fusionpbx-$PKGVER-"$REPO"-wheezy
 
-
-mv *.deb debs-fusionpbx-$PKGVER-"$REPO"-wheezy
-mv *.changes debs-fusionpbx-$PKGVER-"$REPO"-wheezy
-mv *.xz debs-fusionpbx-$PKGVER-"$REPO"-wheezy
-mv *.dsc debs-fusionpbx-$PKGVER-"$REPO"-wheezy
+for i in fusionpbx-core fusionpbx-apps fusionpbx-themes
+do
+mv *.deb debs-fusionpbx-"$PKGVER"-"$REPO"-wheezy
+mv *.changes debs-fusionpbx-"$PKGVER"-"$REPO"-wheezy
+mv *.xz debs-fusionpbx-"$PKGVER"-"$REPO"-wheezy
+mv *.dsc debs-fusionpbx-"$PKGVER"-"$REPO"-wheezy
+done
 
 cp -rp "$WRKDIR"/debs-fusionpbx-"$PKGVER"-"$REPO"-wheezy/* "$REPO"/incoming
 
