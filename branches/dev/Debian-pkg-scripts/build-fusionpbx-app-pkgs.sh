74bb6874fd636bb1811b32e24d2752067b6ac221
r.neese@gmail.com <r.neese@gmail.com@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2014-06-06 02:45:17 +0000

    re writing script. and cleaning it up for better build performance

--- branches/dev/Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
+++ branches/dev/Debian-pkg-scripts/build-fusionpbx-app-pkgs.sh
@@ -1,157 +1,166 @@
 #!/bin/bash
-PKGVER=3.5-89 # this is the version number you update
+
+# Thur June 5, 2014 Time: 9:43 CST
+
+# Select if to build stable/devel pkgs
+BUILD_STABLE_PKGS="n"
+
+#Set Pkg Version Number for build
+PKGVER=3.5-90 # this is the version number you update
+
+#Set Timestamp in the change logs
 TIME=$(date +"%a, %d %b %Y %X")
-STABLE=/usr/home/repo/deb/debian
-DEVEL=/usr/home/repo/deb-dev/debian
-REPO=$DEVEL
+
+if [[ $BUILD_STABLE_PKGS == "y" ]]; then
+SVN_SRC=http://fusionpbx.googlecode.com/svn/trunk
+REPO=/usr/home/repo/deb/debian
+else
+SVN_SRC=http://fusionpbx.googlecode.com/svn/branches/dev
+REPO=/usr/home/repo/deb-dev/debian
+fi
+
 WRKDIR=/usr/src/fusionpbx-pkgs-build
 
 rm -rf /usr/src/fusionpbx-pkgs-build
 
-build_stable_pkgs="n"
-
-if [[ $build_stable_pkgs == "y" ]]; then
-svn_src=http://fusionpbx.googlecode.com/svn/trunk
-else
-svn_src=http://fusionpbx.googlecode.com/svn/branches/dev
-fi
-
 #get pkg system scripts
 svn export http://fusionpbx.googlecode.com/svn/branches/dev/Debian-pkg-scripts "$WRKDIR"
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for core
 cat > "$WRKDIR"/fusionpbx-core/debian/changelog << DELIM
 fusionpbx-core ($PKGVER) stable; urgency=low
 
   * new deb pkg for fusionpbx-core
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for conf files
 cat > "$WRKDIR"/fusionpbx-conf/debian/changelog << DELIM
 fusionpbx-conf ($PKGVER) stable; urgency=low
 
   * new deb pkg for fusionpbx-conf
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for scripts
 cat > "$WRKDIR"/fusionpbx-scripts/debian/changelog << DELIM
 fusionpbx-scripts ($PKGVER) stable; urgency=low
 
   * new deb pkg for fusionpbx-scripts
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for sounds 
 cat > "$WRKDIR"/fusionpbx-sounds/debian/changelog << DELIM
 fusionpbx-sounds ($PKGVER) stable; urgency=low
 
   * new deb pkg for fusionpbx-sounds
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for sqldb 
 cat > "$WRKDIR"/fusionpbx-sql/debian/changelog << DELIM
 fusionpbx-sqldb ($PKGVER) stable; urgency=low
 
   * new deb pkg for fusionpbx-sqldb
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 
-#SET Version nmbr in debian/changelog
+##set version in the changelog files for provisioing templates
 for i in aastra cisco grandstream linksys panasonic polycom snom yealink
 do cat > "$WRKDIR"/fusionpbx-templates/fusionpbx-provisioning-template-"${i}"/debian/changelog << DELIM
-fusionpbx-provisioning-template-${i} ($PKGVER) stable; urgency=low
+fusionpbx-provisioning-template-"${i}" ($PKGVER) stable; urgency=low
 
-  * new deb pkg for fusionpbx-provisioning-template-${i//_/-}
+  * new deb pkg for fusionpbx-provisioning-template-"${i//_/-}"
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 done
 
+#set version in the changelog files for themes
 for i in accessible classic default enhanced nature
 do cat > "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i}"/debian/changelog << DELIM
 fusionpbx-theme-${i} ($PKGVER) stable; urgency=low
 
-  * new deb pkg for fusionpbx-theme-${i//_/-}
+  * new deb pkg for fusionpbx-theme-"${i//_/-}"
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 done
 
+#set version in the changelog files for apps
 for i in adminer call_block call_broadcast call_center call_center_active call_flows calls \
-calls_active click_to_call conference_centers conferences conferences_active contacts content \
-destinations devices dialplan dialplan_inbound dialplan_outbound edit exec extensions fax fifo \
-fifo_list follow_me gateways hot_desking ivr_menu login log_viewer meetings modules music_on_hold \
-park provision recordings registrations ring_groups schemas services settings sipml5 sip_profiles \
-sip_status sql_query system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr \
-xmpp
-do cat > "$WRKDIR"/fusionpbx-apps/fusionpbx-app-${i//_/-}/debian/changelog << DELIM
+	calls_active click_to_call conference_centers conferences conferences_active contacts content \
+	destinations devices dialplan dialplan_inbound dialplan_outbound edit exec extensions fax fifo \
+	fifo_list follow_me gateways hot_desking ivr_menu login log_viewer meetings modules music_on_hold \
+	park provision recordings registrations ring_groups schemas services settings sipml5 sip_profiles \
+	sip_status sql_query system time_conditions traffic_graph vars voicemail_greetings voicemails xml_cdr \
+	xmpp
+do cat > "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/debian/changelog << DELIM
 fusionpbx-app-${i//_/-} ($PKGVER) stable; urgency=low
 
-  * new deb pkg for fusionpbx-app-${i//_/-}
+  * new deb pkg for fusionpbx-app-"${i//_/-}"
 
- -- Richard Neese <r.neese@gmail.com>  $TIME -0500
+ -- Richard Neese <r.neese@gmail.com>  $TIME -0600
 
 DELIM
 done
 
 #get src for apps
 for i in adminer call-block call-broadcast call-center call-center-active call-flows calls \
-calls-active click-to-call conference-centers conferences conferences-active contacts content \
-destinations devices dialplan dialplan-inbound dialplan-outbound edit exec extensions fax fifo \
-fifo-list follow-me gateways hot-desking ivr-menu login log-viewer meetings modules music-on-hold \
-park provision recordings registrations ring-groups schemas services settings sipml5 sip-profiles \
-sip-status sql-query system time-conditions traffic-graph vars voicemail-greetings voicemails xml-cdr \
-xmpp
-do svn export "$svn_src"/fusionpbx/app/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
+	calls-active click-to-call conference-centers conferences conferences-active contacts content \
+	destinations devices dialplan dialplan-inbound dialplan-outbound edit exec extensions fax fifo \
+	fifo-list follow-me gateways hot-desking ivr-menu login log-viewer meetings modules music-on-hold \
+	park provision recordings registrations ring-groups schemas services settings sipml5 sip-profiles \
+	sip-status sql-query system time-conditions traffic-graph vars voicemail-greetings voicemails xml-cdr \
+	xmpp
+do svn export "$SVN_SRC"/fusionpbx/app/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
 done
 
-#Extra Appss
+#get src for extra apps
 for i in sipml5
-do svn export "$svn_src"/apps/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
+do svn export "$SVN_SRC"/apps/"${i}" "$WRKDIR"/fusionpbx-apps/fusionpbx-app-"${i//_/-}"/"${i}"
 done
 
 #get src for core
-svn export --force "$svn_src"/fusionpbx "$WRKDIR"/fusionpbx-core
+svn export --force "$SVN_SRC"/fusionpbx "$WRKDIR"/fusionpbx-core
 for i in app/* themes/* resources/templates/provision resources/templates/conf resources/install
 do rm -rf "$WRKDIR"/fusionpbx-core/"${i}"
 done
 
 #conf dir src
-svn export --force "$svn_src"/fusionpbx/resources/templates/conf "$WRKDIR"/fusionpbx-conf/conf
+svn export --force "$SVN_SRC"/fusionpbx/resources/templates/conf "$WRKDIR"/fusionpbx-conf/conf
 
 #scripts dir src
-svn export --force "$svn_src"/fusionpbx/resources/install/scripts "$WRKDIR"/fusionpbx-scripts/scripts
+svn export --force "$SVN_SRC"/fusionpbx/resources/install/scripts "$WRKDIR"/fusionpbx-scripts/scripts
 
 #sounds dir src
-svn export --force "$svn_src"/fusionpbx/resources/install/sounds "$WRKDIR"/fusionpbx-sounds/sounds
+svn export --force "$SVN_SRC"/fusionpbx/resources/install/sounds "$WRKDIR"/fusionpbx-sounds/sounds
 
 #scripts dir src
-svn export --force "$svn_src"/fusionpbx/resources/install/sql "$WRKDIR"/fusionpbx-sql/sql
+svn export --force "$SVN_SRC"/fusionpbx/resources/install/sql "$WRKDIR"/fusionpbx-sql/sql
 
 #phone provisioing templates
 for i in aastra cisco grandstream linksys panasonic polycom snom yealink
-do svn export "$svn_src"/fusionpbx/resources/templates/provision/"${i}" "$WRKDIR"/fusionpbx-templates/fusionpbx-provisioning-template-"${i}"/"${i}"
+do svn export "$SVN_SRC"/fusionpbx/resources/templates/provision/"${i}" "$WRKDIR"/fusionpbx-templates/fusionpbx-provisioning-template-"${i}"/"${i}"
 done
 
 #get src for theme
 for i in accessible classic default enhanced nature
-do svn export "$svn_src"/fusionpbx/themes/"${i}" "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i}"/"${i}"
+do svn export "$SVN_SRC"/fusionpbx/themes/"${i}" "$WRKDIR"/fusionpbx-themes/fusionpbx-theme-"${i}"/"${i}"
 done
 
 #patch config files
@@ -200,7 +209,7 @@ dpkg-buildpackage -rfakeroot -i
 cd "$WRKDIR"/fusionpbx-sounds
 dpkg-buildpackage -rfakeroot -i
 
-#Build sounds pkg
+#Build sql pkg
 cd "$WRKDIR"/fusionpbx-sql
 dpkg-buildpackage -rfakeroot -i
 
